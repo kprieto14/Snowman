81c31500e9e23a9497dd1e17582707f00d28f9aa
Kristy Prieto <kprieto14@gmail.com>
2023-09-28 20:36:46 -0400

    Update static changes with new gif and looping to create ABC letters/ empty letters for word

--- src/App.tsx
+++ src/App.tsx
@@ -1,61 +1,70 @@
 import React from 'react'
 //import step_0 from '../public/images/step_0.png'
+//import step_1 from '../public/images/step_1.png'
+//import step_2 from '../public/images/step_2.png'
+//import step_3 from '../public/images/step_3.png'
+//import step_4 from '../public/images/step_4.png'
+//import step_5 from '../public/images/step_5.png'
+//import step_6 from '../public/images/step_6.png'
 import step_7 from '../public/images/step_7.png'
+import snow from '../public/images/snow.gif'
+//import words from './words.json'
 
 export function App() {
+  const abcList = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+  //Create a "word" type?
+  const correctWord = ['', '', '', '', '', '', '']
+
+  function handleNewGame() {
+    console.log("Clicked!")
+
+    //Grab a random word from json list
+    //console log the new word to double check it works
+    //Assign its value to an array
+    //Will need to update something so that users know a game started
+  }
+
+  function handleLetterCheck(event: React.MouseEvent<HTMLButtonElement, MouseEvent>) {
+    console.log(event.target)
+
+    //Take in input from what the user clicked
+    //Compare it to the array of the chosen word
+    //Show the correct letters guessed in the correct spot
+    //Disable letters guessed whether it was correct or not
+    //Update snowman picture
+  }
+
   return (
     <div>
       <header>
         <h1>Snowman</h1>
         <h2>
-          <button>New Game</button>
+          <button onClick={handleNewGame}>New Game</button>
         </h2>
       </header>
 
       <main>
         <div>
-          <iframe src="https://giphy.com/embed/rcUnyhf25Bu1y" frameBorder="0" className="giphy-embed" allowFullScreen></iframe>
-          <img src={step_7} alt="Image of a fully built snowman with all parts" />
-          <p><a href="https://giphy.com/stickers/snow-rcUnyhf25Bu1y">via GIPHY</a></p>
+          <iframe src={snow} className="gif"></iframe>
+          <img src={step_7} alt="Image of snowman" />
         </div>
 
         <section>
-          <article><h3>R</h3></article>
-          <article><h3>A</h3></article>
-          <article><h3>N</h3></article>
-          <article><h3>D</h3></article>
-          <article><h3>O</h3></article>
-          <article><h3>M</h3></article>
-          <article><h3>S</h3></article>
+        {correctWord.map((letter, letterIndex) => {
+            return (
+              <article key={letterIndex}><h3>{letter}</h3></article>
+            )
+          })
+        }
         </section>
 
         <section className='alphabet'>
-          <button>A</button>
-          <button>B</button>
-          <button>C</button>
-          <button>D</button>
-          <button>E</button>
-          <button>F</button>
-          <button>G</button>
-          <button>H</button>
-          <button>I</button>
-          <button>J</button>
-          <button>K</button>
-          <button>L</button>
-          <button>M</button>
-          <button>N</button>
-          <button>O</button>
-          <button>P</button>
-          <button>Q</button>
-          <button>R</button>
-          <button>S</button>
-          <button>T</button>
-          <button>U</button>
-          <button>V</button>
-          <button>W</button>
-          <button>X</button>
-          <button>Y</button>
-          <button>Z</button>
+          {abcList.map((letter, letterIndex) => {
+            return (
+              <button key={letterIndex} onClick={handleLetterCheck}>{letter}</button>
+              )
+            })
+          }
         </section>
       </main>
 
